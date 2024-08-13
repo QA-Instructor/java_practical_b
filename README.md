d523425ca7c98383e5e0e55f86bfc29e84560211
Victoria Lloyd <QAInstructorSky@gmail.com>
2024-08-13 10:03:50 +0100

    Added generics challenges and README.md

---
+++ README.md
@@ -0,0 +1,25 @@
+# Java Practical B
+
+## Java Generics
+
+---
+### Examples
+
+List examples:
+
+- nonGenericListExample();
+- genericListExample();
+
+Method Examples
+
+- nonGenericMethodExample();
+- genericMethodExample();
+
+---
+### Generics Challenges
+
+- Challenge 1: Compare arrays
+- Challenge 2: Sum odds and evens
+- Challenge 3: Reverse list
+- Challenge 4: Filter list
+
--- src/main/java/com/qa/GenericsChallenges.java
+++ src/main/java/com/qa/GenericsChallenges.java
@@ -3,5 +3,25 @@ package com.qa;
 public class GenericsChallenges {
 //    Work your way through the following challenges.
 
+//Todo 1: Compare arrays
+//    Write a Java generic method that takes two arrays of the same type and
+//    checks if they have the same elements in the same order.
+//    This method should return a boolean.
+
+
+//Todo 2: Sum odds and evens
+//    Write a Java generic method that takes a list of numbers
+//    and prints the sum of all the even and odd numbers.
+//    This method should work with integers and doubles.
+
+
+//Todo 3: Reverse list
+//    Write a Java generic method that takes a list of any type and
+//    returns it as a new list with the elements in reverse order.
+
+
+//Todo 4: Filter list
+//    Write a Java generic method that takes a list of any type and a predicate.
+//    It should return an array list containing only elements that satisfy the predicate.
 
 }
